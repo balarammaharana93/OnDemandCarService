4df8e4fac1ae92aff8ef7914d3129f8ed74ee54f
Balaram Maharana <Balaram Maharana@Marvel>
2020-06-05 12:50:17 +0530

    Commit Success

---
+++ PaymentGatewayManagement/src/test/java/com/carservice/ondemand/payment/PaymentGatewayManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carservice.ondemand.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
